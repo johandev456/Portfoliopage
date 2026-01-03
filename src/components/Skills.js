3d28d571f1803de286193a8ffe9b9d4794c887e7
johandev456 <johanjohanrosariolopez@gmail.com>
2026-01-03 15:24:22 -0400

    Fix Skills accessibility (alt attributes)

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,62 +1,72 @@
-import { Col,Container,Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from "../assets/meter1.svg";
-import meter2 from "../assets/meter2.svg";
-import meter3 from "../assets/meter3.svg";
-import colorSharp from "../assets/color-sharp.png";
-export const Skills = () => {
-    const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
-return (
-   <section className="skill" id="skills">
-    <Container>
-        <Row>
-            <Col>
-            <div className="skill-bx">
-                <h2>Skills</h2>
-                <p>lorem ipsum dolor sit amet</p>
-                <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                  <div className="item">
-                    <img src={meter1} alt="" />
-                    <h5>Web Development</h5>
-                  </div>
-                  <div className="item" >
-                    <img src={meter2} alt="" />
-                    <h5>Brand Identity</h5>
-                  </div>
-                  <div className="item">
-                    <img src={meter3 } alt="" />
-                    <h5>Logo Design</h5>
-                  </div>
-                  <div className="item">
-                    <img src={colorSharp} alt="" />
-                    <h5>Color Theory</h5>
-                  </div>
-                  </Carousel>
 
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>
+                Technologies and tools I work with to build modern, scalable,
+                and user-friendly applications.
+              </p>
+
+              <Carousel responsive={responsive} infinite className="skill-slider">
+                <div className="item">
+                  <img src={meter1} alt="Frontend development skill level" />
+                  <h5>Frontend Development</h5>
                 </div>
-            </Col>
+
+                <div className="item">
+                  <img src={meter2} alt="Backend development skill level" />
+                  <h5>Backend Development</h5>
+                </div>
+
+                <div className="item">
+                  <img src={meter3} alt="UI and UX design skill level" />
+                  <h5>UI / UX Design</h5>
+                </div>
+
+                <div className="item">
+                  <img src={meter2} alt="Database and data handling skill level" />
+                  <h5>Databases</h5>
+                </div>
+
+                <div className="item">
+                  <img src={meter1} alt="Problem solving and algorithms skill level" />
+                  <h5>Problem Solving</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
         </Row>
-    </Container>
-    <img className="background-image-left" src={colorSharp} />
-        </section>
-)
+      </Container>
+    </section>
+  );
 };
